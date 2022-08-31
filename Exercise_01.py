6bc3907bd79f76607861cc736f06bd64ad7d06b1
Vish <vishagraghav@gmail.com>
2022-08-31 19:40:17 -0400

    Fix the unique list function to actually work

--- Exercise_01.py
+++ Exercise_01.py
@@ -5,7 +5,10 @@ list_02 = [1,1,2,2,3,3,4,5,6,7,8,8]
 def get_unique_list(n_list):
     new_list = []
     for c in n_list:
-        if n_list.count(c) == 1:
+        if c not in new_list:
             new_list.append(c)
     
     print(new_list)
+
+
+get_unique_list(list_02)
