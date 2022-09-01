495d61e7dffb1e88d7097c1c6877c737fab43a30
Vish <vishagraghav@gmail.com>
2022-09-01 10:36:49 -0400

    removed from the list to ensure that they are not counted in the result

--- Exercise_03.py
+++ Exercise_03.py
@@ -3,7 +3,11 @@ inp_01 = input('Enter String: ')
 
 def num_elements(inp):
     dict_01 = {}
-    for i in inp:
+    inp_01 = inp.replace(" ", "")
+    for i in inp_01:
         if i not in dict_01.keys():
             dict_01[i] = inp.count(i) 
     return dict_01
+
+ans = num_elements(inp_01)
+print(ans)
