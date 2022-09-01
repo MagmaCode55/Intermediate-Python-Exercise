a374b331f385b008b5bb5a687e983994bbecca71
Vish <vishagraghav@gmail.com>
2022-09-01 10:45:38 -0400

    Code now will lowercase all letters of the string

--- Exercise_03.py
+++ Exercise_03.py
@@ -1,9 +1,10 @@
-inp_01 = input('Enter String: ')
+inp_01 = (input('Enter String: ')).lower()
 
 
 def num_elements(inp):
     dict_01 = {}
     inp_01 = inp.replace(" ", "")
+    print(inp_01)
     for i in inp_01:
         if i not in dict_01.keys():
             dict_01[i] = inp.count(i) 
